8da18de8080b93b51b217c1d66210917e725097b
Ordencia <ordencia525@gmail.com>
2021-01-15 23:18:54 -0500

    add ip address

--- image_repo/settings.py
+++ image_repo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&(s-%((nmk21^32@iu-)f7hzoz84hsowyl=x%5^i9kgntc-mwf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.227.34.214']
 
 
 # Application definition
